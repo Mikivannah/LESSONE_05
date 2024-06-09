a954fa2d416f086641e78c148c1f8ead3a786c50
Mikivannah <164083395+Mikivannah@users.noreply.github.com>
2024-06-09 19:54:16 +0300

    add new code

--- main.py
+++ main.py
@@ -2,22 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route("/shablon/")
-def films2():
+@app.route("/")
+def films():
     context = {
-
-        "link": "Перейти в кинозал"
+        "link": "Перейти в кинотеатр"
     }
-    return render_template("index.html",  **context)
+    return render_template("index.html", **context)
 
 @app.route("/person/")
 def person():
     context = {
-
-        "link": "Перейти в кинозал"
+        "link": "Перейти в кинотеатр"
     }
-
     return render_template("main.html", **context)
 
 if __name__ == "__main__":
