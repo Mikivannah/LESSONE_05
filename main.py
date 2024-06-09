935071a3e384581bd851488cfd562701b3430479
Mikivannah <164083395+Mikivannah@users.noreply.github.com>
2024-06-09 18:27:18 +0300

    add

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ app = Flask(__name__)
 @app.route("/shablon/")
 def films2():
     context = {
-        "caption": "Гарри Поттер",
+
         "link": "Перейти в кинозал"
     }
     return render_template("index.html",  **context)
 
 @app.route("/person/")
 def person():
-    return render_template("main.html")
+    context = {
+
+        "link": "Перейти в кинозал"
+    }
+
+    return render_template("main.html", **context)
 
 if __name__ == "__main__":
     app.run()
